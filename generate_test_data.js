a8bd562013d96816dc1fbc2fe5420e8ffa6dafef
Malik Kennedy <mksybr@gmail.com>
2020-12-23 10:40:20 -0800

    feat: Generate an arbitrarily large, good looking fake data set

---
+++ generate_test_data.js
@@ -0,0 +1,30 @@
+var faker = require("faker");
+var fs = require('fs');
+
+
+let range = n => [...Array(n).keys()];
+
+function generate_csv(n /*: number*/) {
+  /* => {UUID, String, String (of Number)} */
+
+  let times = range(n);
+  let rows = [];
+  for(time in times) {
+    let uuid = faker.random.uuid(); 
+    let name = faker.name.findName(); 
+    let zip = faker.address.zipCode();
+    rows.push({ uuid: uuid, name: name, zip: zip });
+  }
+  return rows;
+}
+
+
+
+function write_to_file(what, where) {
+  fs.writeFile(where, what, (err) => {
+    if (err) throw err;
+    console.log("There appear to be no errors in writing to ", where);
+  });
+}
+
+write_to_file(JSON.stringify(generate_csv(10000)), "test_data.csv");
