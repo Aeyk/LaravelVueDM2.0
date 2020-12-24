ad3b91e35048766257977ae4f56dabb0d80799c3
Malik Kennedy <mksybr@gmail.com>
2020-12-24 11:30:56 -0800

    chore: WIP begin migrating seperate Vue components into laravel

---
+++ backend/resources/js/components/MailingListIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <b-table striped hover :items="mailing_lists"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MailingListIndex',
+  data()  {
+    return {
+      mailing_lists: [
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+a {
+  color: #42b983;
+}
+</style>
