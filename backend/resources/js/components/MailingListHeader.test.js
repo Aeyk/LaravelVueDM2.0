4c7fd1f448c529508aefd2d7eae109647738ee39
Malik Kennedy <mksybr@gmail.com>
2020-12-28 12:04:53 -0800

    chore+test: rename .vue -> .js + unit test for the header

---
+++ backend/resources/js/components/MailingListHeader.test.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import MailingListHeader from './MailingListHeader.vue'
+
+test('it shows link to upload page', () => {
+  const wrapper = mount(MailingListHeader, {});
+  expect(wrapper.text()).toContain('Mailing List Upload');
+});
+
+test('it shows link to index page', () => {
+  const wrapper = mount(MailingListHeader, {});
+  expect(wrapper.text()).toContain('List of Mailing');
+});
